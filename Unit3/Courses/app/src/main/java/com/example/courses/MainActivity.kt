a4c8e5b5734b1784d0ebaad6dddd6d01c54501aa
hyeonhh <tjdgustjdan@gmail.com>
2023-11-27 01:25:39 +0900

    그리디 course앱 커밋합니다!!
    문제 : 화면에서 원하는사이즈대로 안보인다.
    -> 수정 필요

--- Unit3/Courses/app/src/main/java/com/example/courses/MainActivity.kt
+++ Unit3/Courses/app/src/main/java/com/example/courses/MainActivity.kt
@@ -4,14 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
@@ -19,7 +28,9 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -52,7 +63,13 @@ class MainActivity : ComponentActivity() {
 fun CoursesList(
     topicList:List<Topic>,
     modifier: Modifier=Modifier) {
-    LazyColumn(modifier=modifier){
+    LazyVerticalGrid(
+
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+                modifier=modifier,
+        contentPadding = PaddingValues(8.dp),
+        columns = GridCells.Fixed(2)){
         items(topicList) {
             topic ->
             CoursesCard(topic = topic,
@@ -67,30 +84,35 @@ fun CoursesList(
 fun CourseImage(
     topic: Topic,
     modifier: Modifier=Modifier) {
-    Image(
-        painter = painterResource(topic.imageResourceId),
-        contentDescription = stringResource(topic.stringResourceId),
-        modifier= modifier
-            .height(68.dp)
-            .width(68.dp)
-    )
+    Box {
+        Image(
+            painter = painterResource(topic.imageResourceId),
+            contentDescription = stringResource(topic.stringResourceId),
+            contentScale = ContentScale.Crop,
+            modifier = modifier
+                .aspectRatio(1f)
+                .height(68.dp)
+                .width(68.dp)
+
+        )
+    }
 }
 @Composable
 fun CoursesCard(
     topic: Topic,
     modifier: Modifier=Modifier) {
-    Card(
-        modifier=modifier
-    ) {
+    Card {
         Row {
-            CourseImage(topic = topic)
+            CourseImage(topic)
             Column {
                 Text(
                     text = LocalContext.current.getString(topic.stringResourceId),
+                    style = MaterialTheme.typography.bodySmall,
                     modifier = modifier
                         .padding(start = 16.dp, top = 16.dp, end = 16.dp, bottom = 8.dp)
                 )
-                Row {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically) {
                     Icon(
                         painter = painterResource(id = R.mipmap.ic_grain),
                         contentDescription = null,
@@ -99,6 +121,7 @@ fun CoursesCard(
                     )
                     Text(
                         text = topic.courseCount.toString(),
+                        style=MaterialTheme.typography.labelMedium,
                         modifier = modifier
                             .padding(start = 8.dp)
                     )
